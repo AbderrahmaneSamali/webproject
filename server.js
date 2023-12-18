d321b2e85e2dbc79e9b23797b8d4a0133a53e06d
vega710 <laklaihicham.gl@gmail.com>
2023-12-18 13:26:30 +0100

    first commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send("WHATEVER !!!");
+    res.send("WHATEVER !!! or Hello world");
 });
 
 app.listen(3000)
